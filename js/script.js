d23f65e6cbd4d1975658791ab43da6eef11e7d22
yeri-1002 <yerikang22@gmail.com>
2025-11-20 15:10:37 +0900

    sec-2 진행 중 1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.mySwiper', {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination',
+    type: "progressbar",
+    loop: false,
+  },
+});
