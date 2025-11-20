31b320ec389062a1270801804d0a6392aa205be0
yeri-1002 <yerikang22@gmail.com>
2025-11-21 00:15:39 +0900

    최종 완료

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
+// 스크롤 시 메뉴바
+let lastScrollTop = 0;
+  const menuBar = document.querySelector('.menu-bar');
+
+  window.addEventListener('scroll', function() {
+    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    // 아래로 스크롤 시 메뉴 숨기기
+    if (currentScroll > lastScrollTop && currentScroll > 100) {
+      menuBar.classList.add('hidden');
+    } 
+    // 위로 스크롤 시 메뉴 다시 보이기
+    else {
+      menuBar.classList.remove('hidden');
+    }
+
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // 음수 방지
+  });
+
+
 // 메인 슬라이드
-/*
 const swiperMain = new Swiper('.main-swiper', {
   loop: true,
   autoplay: {
@@ -20,7 +39,6 @@ const swiperMain = new Swiper('.main-swiper', {
     }
   }
 });
-*/
 
 // sec-2 스와이퍼 구현
 const swiperBest = new Swiper('.best-swiper', {
